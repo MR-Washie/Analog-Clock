722358b7e3f59eff209da66f6c8a9074bfab3296
MR-Washie <mdragib.mth2005@gmail.com>
2025-01-29 12:38:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const hourEle = document.querySelector(".hour");
+const minEle = document.querySelector(".min");
+const secEle = document.querySelector(".sec");
+
+
+setInterval (()=>{
+    let date = new Date();
+
+    let hr = date.getHours()*30;
+
+    let min = date.getMinutes()*6;
+
+    let sec = date.getSeconds()*6;
+
+    hourEle.style.transform = `rotateZ(${(hr) + min/12}deg)`
+
+    minEle.style.transform = `rotateZ(${min}deg)`
+
+    secEle.style.transform = `rotateZ(${sec}deg)`
+})
